74dbae4621c014a5d7f3c9b56972fd22b8ad9d9d
Matine temory <temorymatine@gmail.com>
2020-05-20 21:48:08 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,6 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,15 +56,15 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
+ # :known_for => "Ruby",
+  # :languages => ["LISP", "C"]
+
 # return the entire updated hash
 
 	programmer_hash = 
@@ -81,6 +82,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby"} 
+    programmer_hash[:yukihiro_matsumoto] = {:languages => ["Lisp", "C"]}
+    programmer_hash 
+    
 
 end
 
@@ -103,6 +108,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 
 end
 
@@ -125,5 +132,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages] = ["C","Assembly"]
+    programmer_hash
 
 end
