73ccdb4db8ed6992b4e124112ea9a0d33a593cc8
Matine temory <temorymatine@gmail.com>
2020-05-20 21:35:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchies][:languages]
+    programmer_hash[:dennis_ritchie][:languages] = dog
+    dog.to_s
 
 end
 
